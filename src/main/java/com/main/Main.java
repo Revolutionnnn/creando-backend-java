bd9faa001964527516c671ffafbf4d86e396032d
Revolutionnnn <ganandocriptomonedass@gmail.com>
2024-05-09 23:02:36 -0500

    save a update

--- src/main/java/com/main/Main.java
+++ src/main/java/com/main/Main.java
@@ -12,6 +12,17 @@ public class Main {
     public static void main(String[] args) throws SQLException {
         try (Connection myConn = DatabaseConnetion.getInstance()) {
             Repository<Employee> repository = new EmployeeRepository();
+            System.out.println("Insertando");
+            Employee employee = new Employee();
+            employee.setId(4);
+            employee.setFirst_name("Diego");
+            employee.setPa_surname("Pimento");
+            employee.setMa_surname("Gutirre");
+            employee.setEmail("maicol@g.com");
+            employee.setSalary((float) 4000);
+            repository.save(employee);
+
+            System.out.println("Mostrando");
             repository.findAll().forEach(System.out::println);
         }
     }
